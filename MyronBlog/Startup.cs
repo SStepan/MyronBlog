2298b1dc2abc98cb83044441105dc12c37d03228
Stepan Myroniuk <myronyuk.s@gmail.com>
2017-11-15 00:07:47 +0200

    First commit

--- MyronBlog/Startup.cs
+++ MyronBlog/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,14 +16,15 @@ namespace MyronBlog
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            app.UseMvc(config =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                config.MapRoute(
+                    "Default`",
+                    "{controller}/{action}/{id?}",
+                    new {controller = "App", action = "Index"}
+                );
             });
         }
     }
